b2efa9cfb46ee6622d7be90eca55a8ee97975ece
fsarikaya96 <fsarikaya96@hotmail.com>
2023-08-03 22:26:30 +0300

    refactor : file structure changed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
-import Header from './compenents/layout/Header';
-import Footer from './compenents/layout/Footer';
-import Code from './compenents/section-code/Code';
-import Posts from './compenents/section-posts/Posts';
+import Header from './compenents/layout/Public/Header';
+import Footer from './compenents/layout/Public/Footer';
+import Code from './pages/Public/Homepage/Code';
+import Posts from './pages/Public/Homepage/Posts';
 
 function App() {
   
   return (
     <div>
        <Header/>
-       <main className='container mx-auto px-2 sm:px-4 pt-12'>
-        <div className='flex flex-col lg:flex-row justify-between gap-x-8 mb-20'>
-        <Code/>
-        <Posts/>
-        </div>
-      </main>
-       <hr class="container mx-auto mb-4 border-b border-dark"></hr>
+        <main className='container mx-auto px-2 sm:px-4 pt-12'>
+              <div className='flex flex-col lg:flex-row justify-between gap-x-8 mb-20'>
+              <Code/>
+              <Posts/>
+              </div>
+        </main>
+        <hr class="container mx-auto mb-4 border-b border-dark"></hr>
        <Footer/>
   
     </div>
